efee513a4f8de137b477f6cfafeaf22fa2b99594
Hvitrevs <venusof12pm@proton.me>
2024-02-08 00:54:22 -0500

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,12 @@ import ThemeSwitch from "./components/ThemeSwitch";
 
 
 export default function Home() {
-  const { theme, setTheme } = useTheme()
   return (
-    <main className='px-40 py-40'>
-      <h1>
-        Home
-      </h1>
+    <main>
+      <div className='px-[5rem] py-10 dark'>
       <ThemeSwitch />
+      </div>
+      
     </main>
   );
 }
