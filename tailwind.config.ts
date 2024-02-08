80ba3ea219953a99fc8228e6bc5f6bdf70cdd4fc
Hvitrevs <venusof12pm@proton.me>
2024-02-07 19:29:19 -0500

    modified mobile tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,32 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  mode: 'jit',
   theme: {
+    screens: {
+      'xxs': '180px',
+      'xs': '357px',
+      'sm': '540px',
+      'md': '760px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
+      animation: {
+        
+        'bounce': 'bounce 8s ease-in-out infinite',
+      },
+      keyframes: {
+        bounce: {
+          '0%, 100%': {
+            transform: 'translateX(-1%) rotateZ(-2deg) translateY(-1%)',
+          },
+          '50%': {
+            transform: 'translateX(1%) rotateZ(2deg) translateY(1%)',
+          },
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
