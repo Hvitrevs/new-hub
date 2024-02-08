f1dd5f22d8616e4d056b04c76cbe39876340c7a6
Hvitrevs <venusof12pm@proton.me>
2024-02-08 01:13:45 -0500

    bg

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 'use client'
 
-import Image from "next/image";
+import Image from 'next/image';
 
 import { useTheme } from 'next-themes'
-import ThemeSwitch from "./components/ThemeSwitch";
+import ThemeSwitch from './components/ThemeSwitch';
 
 
 export default function Home() {
   return (
-    <main>
-      <div className='px-[5rem] py-10 dark'>
-      <ThemeSwitch />
+    <main className='w-screen h-screen relative'>
+      <div className='flex items-center w-full h-full bg-cover bg-center bg-[#c2d2e5] dark:bg-[#1d213d]'>
+        <div className=' pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]'>
+        <ThemeSwitch />
+        </div>
+        
       </div>
       
     </main>
