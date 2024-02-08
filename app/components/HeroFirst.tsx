945962cf93de6ce5fdf4ffe7c132b03e814a6e51
Hvitrevs <venusof12pm@proton.me>
2024-02-08 05:43:50 -0500

    image added

--- app/components/HeroFirst.tsx
+++ app/components/HeroFirst.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 import React from 'react';
 import ThemeSwitch from './ThemeSwitch'; // Assuming ThemeSwitch is another component you've defined
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 const HeroFirst = () => {
   return (
@@ -23,17 +24,20 @@ const HeroFirst = () => {
           <h4 className='font-bold text-[25px]'>
             I like building web apps
           </h4>
-          <span className='mt-[35px] rounded-lg p-3 border-l-0 bg-gradient-to-b from-[#d0e2f7] to-transparent shadow-2xl dark:border-t-[4px] dark:border-[#202445] dark:shadow-[#171a31] dark:bg-none'>
+          <span className='mt-[15px] rounded-lg px-3 py-5 border-l-0 bg-gradient-to-b from-[#d0e2f7] to-transparent shadow-[#90abbf] shadow-2xl dark:border-t-[4px] dark:border-[#202445] dark:shadow-[#171a31] dark:text-[#5e6393] dark:bg-none'>
             I am fascinated with AI and Machine Learning. Currently I am working on designing and creating different user-friendly websites and applications.
           </span>
 
-          <div className='py-5 flex flex-nowrap gap-4 px-1 text-[20px]'>
-            <Link href='https://github.com/Hvitrevs'>
+          <div className='py-5 flex flex-nowrap gap-4 px-1 text-[25px]'>
+            <Link 
+            className='transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-120 duration-300'  href='https://github.com/Hvitrevs'>
               <FaGithub />
             </Link>
 
-            <div className='gap-4 px-1 text-[20px] text-[#ff7944]'>
-              <Link href="https://codepen.io/Hvitrevs" passHref>
+            <div className='gap-4 px-1 text-[25px] text-[#ff7944] transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-120 duration-300'>
+              <Link 
+              className='' 
+              href="https://codepen.io/Hvitrevs">
                 <FaCodepen />
               </Link>
             </div>
